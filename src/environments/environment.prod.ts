9b1b400cd45c4c528ce5b9afcec19e2e77ff8a8e
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2024-07-19 19:15:36 -0500

    aux

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,8 +7,6 @@ export const environment = {
   response_type: 'code',
   response_mode: 'form_post',
   code_challenge_method: 'S256',
-  //code_challenge: 'HoHpE-5ooPGF-FMtKjra9QYc1Ar-0SqKcBFfx6-xsqM',
-  //code_verifier: 'vgrpHZBHIIxabLFsNarHqHbqWRk2yT10jbQ9kRJFI5x',
   token_url: 'http://service-auth-server.azurewebsites.net/oauth2/token',
   grant_type: 'authorization_code',
   resource_url: 'http://localhost:8080/resource/',
@@ -20,10 +18,7 @@ export const environment = {
   api_documentos: 'http://localhost:9000/api/doc-compra',
   api_coti: 'http://localhost:9000/api/cotizacion',
   api_cliente: 'http://localhost:9000/api/cliente',
-
   api_pedido: 'http://localhost:9000/api/pedido',
   api_factura: 'http://localhost:9000/api/facturaventa',
-
   api_proveedor: 'http://localhost:9000/api/proveedor',
-
 };
